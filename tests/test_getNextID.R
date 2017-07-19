15fbce45972711af56dde7f2a7fa3c2f2fb71eaf
Niall Tyndall <ntyndall@gmail.com>
2017-07-19 19:54:01 +0100

    test that a simple test fails the build.

--- tests/test_getNextID.R
+++ tests/test_getNextID.R
@@ -6,14 +6,7 @@
 # Begin testing  
 test_that("first test", {
   
-  IDLookup <- "match"  
-  getNextID(IDLookup = IDLookup,
-            IDList = c("match"))
-  
-  result <- rredis::redisGet(key = paste0("next:", IDLookup, ":id"))
-  result <- as.integer(result)
-
   expect_that( 1, is_a("numeric"))
-  expect_that( result, equals(1) )
+  expect_that( 2, equals(1) )
   
 })
