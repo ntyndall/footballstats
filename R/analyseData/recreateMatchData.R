b84c09ea91ba0f485a79482bd0c5fc2661b681b7
Niall Tyndall <ntyndall@gmail.com>
2017-07-26 20:33:48 +0100

    First analysis function for recreating the match data structure

---
+++ R/analyseData/recreateMatchData.R
@@ -0,0 +1,37 @@
+#' @title Recreate Match Data
+#' 
+#' @description A function that provided with a competitionID and season start year,
+#'  can auto generate and order by date all the basic match information
+#'  
+#' @details A search for all matches in a particular subset is made in Redis, a data frame
+#'  is then constructed to rebuild the original API query and ordered by date.
+#'  
+#' @param competitionID An integer containing the competitionID that the 
+#'  teams and match information belong to.
+#' @param seasonStarting An integer defining the lower year for details on a season.
+#'  
+#' @return matchData. A data frame containing all the matches in a particular season.
+#' 
+
+
+recreateMatchData <- function(competitionID, seasonStarting) {
+  allMatches <- redis$KEYS(pattern = paste0('comp:season:match:', competitionID, ':', seasonStarting, '*'))
+  matchData <- data.frame(stringsAsFactors = FALSE)
+  for (i in 1:length(allMatches)) {
+    singleMatch <- redis$HGETALL(key = allMatches[i])
+    singleMatch <- as.character(singleMatch)
+  
+    matchID <- data.frame(t(singleMatch[c(FALSE, TRUE)]),
+                          stringsAsFactors = FALSE)
+    matchIDName <- singleMatch[c(TRUE, FALSE)]
+    names(matchID) <- matchIDName
+    if (i == 0) {
+      matchData <- matchID
+    } else {
+      matchData <- rbind(matchData, matchID)
+    }
+  }
+  # Re-order the dataframe by date.
+  matchData$formatted_date <- as.Date(matchData$formatted_date, '%d.%m.%Y')
+  return(matchData[order(matchData$formatted_date), ])
+}
