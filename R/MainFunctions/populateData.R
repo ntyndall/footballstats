474ab7c42259541c65e0e18a00f24813d75b1cef
Niall Tyndall <ntyndall@gmail.com>
2017-07-26 20:27:06 +0100

    Wrappers around main redis storing capability

---
+++ R/MainFunctions/populateData.R
@@ -0,0 +1,22 @@
+#
+#  Run script to populate Redis.
+#
+
+source(paste0(getwd(), '/Desktop/football-project/footballstats/R/MainFunctions/storeData.R'))
+source(paste0(getwd(), '/Desktop/football-project/footballstats/R/UtilityFunctions/initialize.R'))
+
+# Load competitions and run the functionality below. 
+# (Figure out competition ID's with a single GET request first!)
+#competitions <- addCompetitionInfo()
+comps <- jsonlite::fromJSON(seasonIDs)
+seasonYear <- c(2013, 2014, 2015, 2016)
+
+for (i in 1:nrow(comps)) {
+  for (j in 1:length(seasonYear)) {
+    print(paste0('looking at season ', seasonYear[j], '/', seasonYear[j] + 1, 
+                 ' for competition - ', comps$name[i]))
+    storeData(compID = comps$id[i], 
+              seasonStarting = seasonYear[j],
+              updateData = FALSE)
+  }
+}
---
+++ R/MainFunctions/storeData.R
@@ -0,0 +1,31 @@
+#' @title Store Data
+#'
+#' @description A function that loads all functionality and stores all relevant
+#'  information to Redis
+#'  
+#' @details 
+#'  (1) Load all functions
+#'  (2) Import ALL data with autoAnalyse = FALSE
+#'  (3) Update relevant data with autoAnalyse = TRUE
+#'  (4) Set start seasonDate e.g. seasonStarting = 2015 for 2015/2016 season
+#'  (5) Provide Redis database configured in (1)
+#'  
+#' @param compID An integer containing the competitionID that the 
+#'  teams and match information belong to.
+#' @param seasonStarting An integer defining the lower year for details on a season.
+#' @param updateData A boolean to signify whether information is only to be looked at
+#'  for the first time or whether data needs updated (i.e. a new match has been played.)
+#'  
+
+
+storeData <- function(compID, seasonStarting, updateData) {
+  initialize(location = paste0(getwd(), '/Desktop/football-project/footballstats/R/'),
+             redisHost = 'localhost',
+             redisPort = 6379,
+             db = 10)
+  
+  mainController(redis = redis,
+                 compID = compID, 
+                 updateData = updateData, 
+                 seasonStarting = seasonStarting)
+}
