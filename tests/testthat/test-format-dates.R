ce9c50049a668e9d171711b79ea244e017978fcf
Niall Tyndall <ntyndall@gmail.com>
2017-11-06 19:38:55 +0000

    trying again with the test...

--- tests/testthat/test-format-dates.R
+++ tests/testthat/test-format-dates.R
@@ -7,8 +7,8 @@ test_that("Convert a simple date to one used by the API", {
 
   components <- strsplit(x = res, split = '[.]')[[1]]
 
-  expect_equal( components[1], equals('06') )
-  expect_equal( components[2], equals('11') )
-  expect_equal( components[3], equals('2017') )
+  expect_that( components[1], equals('06') )
+  expect_that( components[2], equals('11') )
+  expect_that( components[3], equals('2017') )
 })
 
