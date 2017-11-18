5c1702e4c8ef89af1af147a62167d5ac12ec6f01
Niall Tyndall <ntyndall@gmail.com>
2017-11-18 22:11:15 +0000

    Formatting for get_data...

--- R/get_data.R
+++ R/get_data.R
@@ -2,24 +2,30 @@
 #'
 #' @description A function that generates JSON data from a particular
 #'  endpoint provided to the function as a string.
-#'  
+#'
 #' @import httr
-#' 
+#'
 #' @param endpoint A character string that defines the endpoint and parameters
-#'  utilized to gather the data. The endpoint is accessed by concatenating the 
+#'  utilized to gather the data. The endpoint is accessed by concatenating the
 #'  string HOST -> endpoint -> apiKey.
 #' @param apiKey An alphanumeric value that contains a given API_KEY
 #'  which is loaded into the global environment to allow access to the endpoint.
 #' @param host An alphanumeric value that contains a given HOST
 #'  which is loaded into the global environment to allow access to the endpoint.
-#'  
+#'
 #' @return A list of data corresponding to the `endpoint` if status_code == 200
 #' @return Null for an appropriate response if status_code != 200
 #'
 
 
 get_data <- function(endpoint, KEYS) {
-  listOfSeasons <- httr::GET(paste0(KEYS$FS_HOST, endpoint, KEYS$FS_APIKEY))
-  return(listOfSeasons$status_code %>% 
-    purrr::when(. == 200 ~ jsonlite::fromJSON(rawToChar(listOfSeasons$content)), ~ NULL))
+  listOfSeasons <- httr::GET(
+    url = paste0(
+      KEYS$FS_HOST, endpoint, KEYS$FS_APIKEY))
+  return(listOfSeasons$status_code %>%
+    purrr::when(
+      . == 200 ~ listOfSeasons$content %>%
+        rawToChar() %>%
+          jsonlite::fromJSON(),
+      ~ NULL))
 }
