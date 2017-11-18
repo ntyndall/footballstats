f49af8883e904637623bbba5f5c3e29fc68e6062
Niall Tyndall <ntyndall@gmail.com>
2017-11-18 22:12:04 +0000

    initial testing for team information

---
+++ tests/testthat/test-ateam_info.R
@@ -0,0 +1,54 @@
+context("test-ateam_info.R")
+
+rredis::redisConnect(
+  host = 'localhost',
+  port = 6379)
+rredis::redisSelect(3)
+rredis::redisFlushDB()
+
+test_that("Check the keys are as they should be by adding team data", {
+
+  teamID <- 9002
+  rredis::redisLPush(
+    key = 'analyseTeams',
+    value = teamID)
+
+  expect_that( 'analyseTeams' %>% rredis::redisExists(), is_true() )
+
+  footballstats::ateam_info(
+    competitionID = 1204,
+    teamListLength = 1,
+    updateData = FALSE,
+    KEYS = NULL,
+    bypass = bypass)
+
+  # Check redis for expected output
+  teamIDs <- rredis::redisKeys(
+    pattern = 'ctp:*') %>%
+    strsplit(split = ':') %>%
+    purrr::map(4) %>%
+    purrr::flatten_chr() %>%
+    as.integer() %>%
+    sort()
+
+  # Check the keys exist
+  expect_that( 'ct_basic:1204:9002' %>% rredis::redisExists(), is_true() )
+  expect_that( 'ct_stats:1204:9002' %>% rredis::redisExists(), is_true() )
+  expect_that( 'analyseTeams' %>% rredis::redisExists(), is_false() )
+
+  # Check basic statistics
+  basicStats <- rredis::redisHGetAll(
+    key = 'ct_basic:1204:9002')
+
+  expect_that( basicStats %>% names() %>% length(), equals(14) )
+  expect_that( basicStats$team_id %>% as.integer(), equals(teamID) )
+  expect_that( basicStats$name %>% as.character(), equals('Arsenal') )
+
+  # Check general statistics
+  teamStats <- rredis::redisHGetAll(
+    key = 'ct_stats:1204:9002')
+
+  expect_that( teamStats %>% names() %>% length(), equals(49) )
+  expect_that( teamStats$wins %>% as.integer(), equals(6) )
+
+})
