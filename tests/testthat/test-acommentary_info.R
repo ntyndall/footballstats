79d885e4efe28e3495e518047bf93c57c593785c
Niall Tyndall <ntyndall@gmail.com>
2017-11-25 16:53:27 +0000

    Try out new tests for commentary Info

---
+++ tests/testthat/test-acommentary_info.R
@@ -0,0 +1,68 @@
+context("test-add_info.R")
+
+rredis::redisConnect(
+  host = 'localhost',
+  port = 6379)
+rredis::redisSelect(3)
+rredis::redisFlushDB()
+
+test_that("Send in a single commentary to see it is stored correctly", {
+
+  # Get the match info coupled to the commentary
+  commentaryData <- footballstats::commentaryData[[1]]
+  matches <- footballstats::matchData
+  localteam <- matches$localteam_id[1]
+  visitorteam <- matches$visitorteam_id[1]
+  matchID <- commentaryData[[1]]$match_id
+
+
+  footballstats::acommentary_info(
+    competitionID = '1204',
+    matchIDs = matchID,
+    localteam = localteam,
+    visitorteam = visitorteam,
+    KEYS = KEY,
+    bypass = TRUE)
+
+  playerInfo <- rredis::redisKeys(
+    pattern = 'cmp:*')
+
+  expect_that( playerInfo %>% length(), equals(33) )
+
+  singleMatch <- playerInfo %>%
+    strsplit(split = ':') %>%
+    purrr::map(3) %>%
+    purrr::flatten_chr() %>%
+    unique()
+
+  expect_that( singleMatch, equals(1) )
+
+  checkPlayerExists <- rredis::redisHMGet(
+    key = paste0('cmp:1204:', matchID, ':153493'),
+    fields = 'name')
+
+  expect_that( checkPlayerExists$name %>% as.character() %>% tolower(), equals('mohamed elneny') )
+
+  # Get the general match commentaries
+  teamCommentaries <- rredis::redisKeys(
+    pattern = 'cmt_commentary:*')
+
+  teamIDs <- teamCommentaries %>%
+    strsplit(split = ':') %>%
+    purrr::map(4) %>%
+    purrr::flatten_chr() %>%
+    as.integer() %>%
+    sort()
+
+  expect_that( teamIDs, equals(c(visitorteam, localteam) %>% as.integer() %>% sort()) )
+
+  index <- which(grepl(teamIDs[1], teamCommentaries))
+  teamDetails <- rredis::redisHGetAll(
+    key = teamCommentaries[index])
+
+  expect_that( teamDetails$shots_total %>% as.character() %>% as.integer(), equals(25) )
+  expect_that( teamDetails$shots_ongoal %>% as.character() %>% as.integer(), equals(12) )
+  expect_that( teamDetails$fouls %>% as.character() %>% as.integer(), equals(7) )
+  expect_that( teamDetails$corners %>% as.character() %>% as.integer(), equals(6) )
+
+})
