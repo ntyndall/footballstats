b917cd871b6fea6057cbb029f16bda53248dc3ca
Niall Tyndall <ntyndall@gmail.com>
2017-11-25 15:46:23 +0000

    Updating formatting.

--- R/classify_form_info.R
+++ R/classify_form_info.R
@@ -34,10 +34,17 @@ form_from_match <- function(competitionID, matchIDs, seasonStarting, matchFieldN
     # Need to choose which current team is being analysed for each match.
     currentTeam <- matchFieldNames[as.integer(which(singleMatch == teamID))]
 
-    scoreCurrent <- as.integer(singleMatch[currentTeam %>%
-                                             purrr::when(. == 'localteam_id' ~ 'localteam_score', ~ 'visitorteam_score')])
-    scoreOther <-  as.integer(singleMatch[currentTeam %>%
-                                            purrr::when(. == 'localteam_id' ~ 'visitorteam_score', ~ 'localteam_score')])
+    scoreCurrent <- singleMatch[
+      currentTeam %>%
+      purrr::when(. == 'localteam_id' ~ 'localteam_score',
+                  ~ 'visitorteam_score')] %>%
+      as.integer()
+
+    scoreOther <-  singleMatch[
+      currentTeam %>%
+      purrr::when(. == 'localteam_id' ~ 'visitorteam_score',
+                  ~ 'localteam_score')] %>%
+      as.integer()
 
     # Determine the result of the match for the current team
     singleResult <- footballstats::match_result(
@@ -46,9 +53,10 @@ form_from_match <- function(competitionID, matchIDs, seasonStarting, matchFieldN
     list(singleMatch$formatted_date, singleResult)
   })
 
-  totalForm <- data.frame(date = sapply(1:length(formList), function(k) formList[[k]][[1]] ),
-                          form = sapply(1:length(formList), function(k) formList[[k]][[2]] ),
-                          stringsAsFactors = FALSE)
+  totalForm <- data.frame(
+    date = sapply(1:length(formList), function(k) formList[[k]][[1]] ),
+    form = sapply(1:length(formList), function(k) formList[[k]][[2]] ),
+    stringsAsFactors = FALSE)
 
   totalForm <- totalForm[sort.int(totalForm$date, decreasing = FALSE, index.return = TRUE)$ix, ]
   return(paste(as.character(na.omit(totalForm$form[1:formLimit])), collapse = ''))
@@ -105,12 +113,18 @@ team_form <- function(matchData, teamID) {
     singleMatch <- as.list(singleTeam[x, ])
 
     currentTeam <- singleMatch[as.integer(which(singleMatch == as.character(teamID)))]
-    scoreCurrent <- as.integer(singleMatch[currentTeam %>%
-                                             purrr::when(. == 'localteam_id' ~ 'localteam_score',
-                                                         ~ 'visitorteam_score')])
-    scoreOther <-  as.integer(singleMatch[currentTeam %>%
-                                            purrr::when(. == 'localteam_id' ~ 'visitorteam_score',
-                                                        ~ 'localteam_score')])
+
+    scoreCurrent <- singleMatch[
+      currentTeam %>%
+      purrr::when(. == 'localteam_id' ~ 'localteam_score',
+                  ~ 'visitorteam_score')] %>%
+      as.integer()
+
+    scoreOther <- singleMatch[
+      currentTeam %>%
+      purrr::when(. == 'localteam_id' ~ 'visitorteam_score',
+                  ~ 'localteam_score')] %>%
+      as.integer()
 
     match_result(scoreCurrent =  scoreCurrent,
                  scoreOther = scoreOther)
