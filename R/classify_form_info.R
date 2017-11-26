fceb7325791066f99942ab086b58cb23bf2a96c6
Niall Tyndall <ntyndall@gmail.com>
2017-11-26 13:00:08 +0000

    Moving match_result function plus fixing a major bug in the analysis on the current team.

--- R/classify_form_info.R
+++ R/classify_form_info.R
@@ -122,21 +122,46 @@ team_form <- function(matchData, teamID) {
 
 
 current_or_other <- function(singleMatch, teamID) {
-  currentTeam <- singleMatch[as.integer(which(singleMatch == as.character(teamID)))]
+  currentTeam <- singleMatch[
+    (singleMatch == teamID %>% as.character()) %>% which()] %>% names()
 
   scoreCurrent <- singleMatch[
     currentTeam %>%
-      purrr::when(. == 'localteam_id' ~ 'localteam_score',
-                  ~ 'visitorteam_score')] %>%
+      purrr::when(
+        . == 'localteam_id' ~ 'localteam_score',
+        ~ 'visitorteam_score')] %>%
     as.integer()
 
   scoreOther <- singleMatch[
     currentTeam %>%
-      purrr::when(. == 'localteam_id' ~ 'visitorteam_score',
-                  ~ 'localteam_score')] %>%
+      purrr::when(
+        . == 'localteam_id' ~ 'visitorteam_score',
+        ~ 'localteam_score')] %>%
     as.integer()
 
   return(list(current = scoreCurrent, other = scoreOther))
 }
 
+#' @title classify_match_result
+#'
+#' @description A function that returns a single character value of
+#'  'W' / 'L' / 'D' depending on the scores and which team scored them.
+#'
+#' @param scoreHome An integer value denoting the home team score.
+#' @param scoreAway An integer value denoting the away team score.
+#' @param homeOrAway A character vector which is either 'localteam_id'
+#'  or 'visitorteam_id', to conduct the correct operation on the two
+#'  score parameters.
+#'
+#' @return Returns one of 'W' / 'L' / 'D'.
+#'
+#' @export
 
+
+match_result <- function(scoreCurrent, scoreOther) {
+  return(c(scoreCurrent, scoreOther) %>%
+           purrr::when(
+             .[1] == .[2] ~ 'D',
+             .[1] > .[2] ~ 'W',
+             'L'))
+}
--- R/classify_utils.R
+++ R/classify_utils.R
@@ -183,27 +183,3 @@ commentary_from_redis <- function(keyName, returnItems) {
   }
   return(as.double(results))
 }
-
-#' @title classify_match_result
-#'
-#' @description A function that returns a single character value of
-#'  'W' / 'L' / 'D' depending on the scores and which team scored them.
-#'
-#' @param scoreHome An integer value denoting the home team score.
-#' @param scoreAway An integer value denoting the away team score.
-#' @param homeOrAway A character vector which is either 'localteam_id'
-#'  or 'visitorteam_id', to conduct the correct operation on the two
-#'  score parameters.
-#'
-#' @return Returns one of 'W' / 'L' / 'D'.
-#'
-#' @export
-
-
-match_result <- function(scoreCurrent, scoreOther) {
-  return(c(scoreCurrent, scoreOther) %>%
-           purrr::when(
-             .[1] == .[2] ~ 'D',
-             .[1] > .[2] ~ 'W',
-             'L'))
-}
