273382c260de4577ced0378929e5d58ac06149b1
Niall Tyndall <ntyndall@gmail.com>
2018-01-07 16:04:35 +0000

    New ideas for building model dataset

--- R/classify_calculate_svm.R
+++ R/classify_calculate_svm.R
@@ -16,53 +16,57 @@
 #' @export
 
 
-calculate_svm <- function(competitionID, seasonStarting, commentaryKeys,
-                          commentaryNames, matchData,
-                          totalData = data.frame(stringsAsFactors = FALSE)) {
+calculate_svm <- function(commentaryNames, matchData) {
 
-  for (i in 1:length(commentaryKeys)) {
-    # Parse out commentary items and create a single row data frame
-    singleCommentary <- commentaryKeys[i]
-    elementsSplit <- singleCommentary %>% strsplit(split = ':') %>% purrr::flatten_chr()
-    matchID <- elementsSplit[3]
-    teamID <- elementsSplit[4]
+  # Infer competitionID
+  competitionID <- matchData$comp_id %>% footballstats::prs_comp()
+  seasonStarting <- matchData$season %>% footballstats::prs_season()
+  mDat <- data.frame(stringsAsFactors = FALSE)
 
-    # Get Commentary results from Redis
-    results <- footballstats::commentary_from_redis(
-      keyName = singleCommentary,
-      returnItems = commentaryNames)
-
-    # Create single row of information
-    singleItem <- data.frame(t(results), stringsAsFactors = FALSE)
-    names(singleItem) <- commentaryNames
-
+  for (i in 1:nrow(matchData)) {
+    matchID <- matchData$id[i]
     # Get single match information
     singleMatchInfo <- rredis::redisHGetAll(
       key = paste0('csm:', competitionID, ':', seasonStarting, ':', matchID))
+    teamIDs <- c(matchData$localteam_id[i], matchData$visitorteam_id[i])
 
-    # Need to choose which current team is being analysed for each match.
-    currentTeam <- singleMatchInfo[as.integer(which(singleMatchInfo == teamID))]
-    scoreCurrent <- singleMatchInfo[ifelse(
-      currentTeam == 'localteam_id',
-      yes = 'localteam_score',
-      no = 'visitorteam_score')] %>%
-      as.integer()
+    datSlice <- footballstats::build_model(
+      competitionID = competitionID,
+      matchID = matchID,
+      teamIDs = teamIDs,
+      commentaryNames = commentaryNames,
+      matchData = matchData,
+      singleMatchInfo = singleMatchInfo)
 
-    scoreOther <-  singleMatchInfo[ifelse(
-      currentTeam == 'localteam_id',
-      yes = 'visitorteam_score',
-      no = 'localteam_score')] %>%
-      as.integer()
+    mDat %<>% rbind(datSlice)
+  }
 
-    # Get the result of the match whether it was a home or away game
-    winLoseDraw <- footballstats::match_result(
-      scoreCurrent = scoreCurrent,
-      scoreOther = scoreOther)
+  mDat %>%
+    footballstats::drop_unique_feats() %>%
+    return()
+}
 
-    # Calculate team form (Don't include if 3 matches don't exist yet!)
+#'
+#' @export
+
+
+build_model <- function(competitionID, matchID, teamIDs, commentaryNames, matchData, singleMatchInfo) {
+
+  returnData <- data.frame(stringsAsFactors = FALSE)
+  for (j in 1:2) {
+    commentaryKey <- paste0('cmt_commentary:', competitionID, ':', matchID, ':', teamIDs[j]) %>%
+      rredis::redisKeys() %>% as.character
+
+    if (identical(commentaryKey, character(0))) break
+    # Get Commentary results from Redis
+    results <- footballstats::commentary_from_redis(
+      keyName = commentaryKey,
+      returnItems = commentaryNames)
+
+    # Calculate team form
     formResults <- footballstats::team_form(
       matchData = matchData,
-      teamID = teamID)
+      teamID = teamIDs[j])
 
     # Create a data frame of forms and dates.
     totalForm <- data.frame(
@@ -75,29 +79,31 @@ calculate_svm <- function(competitionID, seasonStarting, commentaryKeys,
       matchInfo = singleMatchInfo,
       totalForm = totalForm)
 
-    # Only if the match has seen 3 previous games do we add a row to the totalData frame
-    # This keeps the forms consistent to the previous 3 matches!
-    if (!is.null(form)) {
-      # Calculate additional metrics
-      #singleItem <- calculateAdditionalMetrics(competitionID = competitionID,
-      #                                         teamID = teamID,
-      #                                         seasonStarting = seasonStarting,
-      #                                         singleItem = singleItem)
+    if (is.null(form)) {
+     break
+    } else {
+      if (j == 1) {
+        fRes <- results
+        fFirst <- form %>% footballstats::form_to_int()
+      } else {
+        winLoseDraw <- footballstats::match_result(
+          scoreCurrent = singleMatchInfo$localteam_score %>% as.integer,
+          scoreOther = singleMatchInfo$visitorteam_score %>% as.integer)
+        print(fRes - results)
 
-      # Append the form and results to single row data
-      singleItem$form <- form
-      singleItem$res <- winLoseDraw
+        # Create single row of information
+        singleItem <- data.frame(t(fRes - results), stringsAsFactors = FALSE)
+        names(singleItem) <- commentaryNames
 
-      # Bind the data frames together into one
-      totalData <- rbind(totalData, singleItem)
+        # Append form and result to data frame
+        singleItem$form <- `-`(fFirst, form %>% footballstats::form_to_int())
+        singleItem$res <- winLoseDraw
+
+        # Bind the data frames together into one
+        returnData <- singleItem
+      }
     }
   }
 
-  # Drop any single valued metrics
-  frameNames <- totalData %>% names
-  FALSE %>% rep(frameNames %>% length) %>% as.list
-  sums <- sapply(totalData, unique) %>% lengths(use.names = FALSE)
-  toDrop <- `<`(sums, 2)
-
-  return(if (toDrop %>% any) totalData %>% subset(select = -c(toDrop %>% which)) else totalData)
+  returnData %>% return()
 }
