502b603023dc12946de32fb17a03392c33995305
Niall Tyndall <ntyndall@gmail.com>
2018-01-20 22:00:39 +0000

    Update tag functionality to take the most recent tag on github...

--- tag.R
+++ tag.R
@@ -12,38 +12,50 @@ descFileNames <- names(descFile)
 if (descFileNames %>% purrr::has_element('Version')) {
   ver <- descFile[['Version']]
 } else {
-  message(' -- Cannot find a valid _Version_ key in the DESCRIPTION file --')
-  stop(' END (unsuccessful)')
+  message(' ## Cannot find a valid _Version_ key in the DESCRIPTION file --')
+  stop(' ## STOPPING --')
 }
 
+# Find the current tags from github
 current <- 'git tag' %>%
-  system(
-    intern = TRUE) %>%
+  system(intern = TRUE) %>%
     gsub(
       pattern = 'v',
       replacement = '')
 
-verSplit <- (ver %>% strsplit(split = '[.]'))[[1]]
-currentSplit <- (current %>% strsplit(split = '[.]'))[[1]]
+# Split version in description
+verSplit <- ver %>%
+  strsplit(split = '[.]') %>%
+  purrr::flatten_chr()
 
-verLen <- verSplit %>% length()
-currentLen <- currentSplit %>% length()
+# Split current git tag
+currentSplit <- current[current %>% length] %>%
+  strsplit(split = '[.]') %>%
+  purrr::flatten_chr()
+
+# Get their respective lengths
+verLen <- verSplit %>% length
+currentLen <- currentSplit %>% length
+
+# Align the lengths if they are off by major . minor . path versioning style
 if (verLen > currentLen) {
   currentSplit <- c(currentSplit, rep(0, (verLen - currentLen)))
 } else if (verLen < currentLen) {
   verSplit <- c(verSplit, rep(0, (currentLen - verLen)))
 }
 
+# Compare the two once they have been cleaned up
 compare <- utils::compareVersion(
   a = paste(verSplit, collapse = '.'),
   b = paste(currentSplit, collapse = '.'))
 
+# Print to screen what is about to happen
 if (compare == 0) {
-  message(' -- Version hasnt changed between current tag and DESCRIPTION file--')
-  stop(' END (unsuccessful) ')
+  message(' ## Version has not changed between current tag and DESCRIPTION file--')
+  stop(' ## STOPPING -- ')
 } else if (compare < 0) {
-  message(' -- Version is somehow lower in the DESCRIPTION file --')
-  stop(' END (unsuccessful) ')
+  message(' ## Version is somehow lower in the DESCRIPTION file --')
+  stop(' ## STOPPING -- ')
 }
 
 # Once versioning has been approved by the checks then create the tar and move it to /tagged/
@@ -51,17 +63,20 @@ devtools::build(
   pkg = dir,
   binary = TRUE)
 
+# ... find the built tar
 latestRelease <- list.files(
   path = '../',
   pattern = '*.tar.gz')
 
+# ... rename file to latest release and move to /tagged/
 renameSuccess <- file.rename(
   from = paste0('../', latestRelease),
   to = paste0('tagged/', latestRelease))
 
+# If successful then print to screen
 if (renameSuccess) {
-  message(paste0(' -- Tagged latest release successfully from [ ', current, ' ] to [ ', ver, ' ] -- '))
-  message(paste0(' -- Double check the versions are as required and complete the tagging on github! -- '))
+  message(paste0(' ## Tagged latest release successfully from [ ', current, ' ] to [ ', ver, ' ] -- '))
+  message(paste0(' ## Double check the versions are as required and complete the tagging on github! -- '))
 } else {
-  message(' -- Warning! Could not tag release -- ')
+  message(' ## WARNING! Could not tag release -- ')
 }
