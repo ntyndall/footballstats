f591faa3f1791b50146380cee278298b07851f53
Niall Tyndall <ntyndall@gmail.com>
2018-01-27 20:22:15 +0000

    update creation of nn

--- demo/create_nn.R
+++ demo/create_nn.R
@@ -8,24 +8,34 @@ comps <- footballstats::allowed_comps()
 cat(paste0(Sys.time(), ' | Recreating match data. \n'))
 totalData <- data.frame(stringsAsFactors = FALSE)
 for (i in 1:(comps %>% length)) {
+  competitionID <- comps[i]
   matchData <- footballstats::recreate_matchdata(
-    competitionID = comps[i],
+    competitionID = competitionID,
     seasonStarting = seasonStarting,
     matchLimit = 10000)
   totalData %<>% rbind(matchData)
 }
+
+# Build league table
+totalData %>% footballstats::create_table()
+
+# Store positions on a weekly basis
+footballstats::weekly_positions(
+  competitionID = competitionID,
+  seasonStarting = 2017
+)
 
 # Construct data set for building an SVM
 cat(paste0(Sys.time(), ' | Creating a dataframe from the match data. \n'))
 original.data <- totalData %>% footballstats::calculate_data()
 
 # Calculate relative position
-subD <- totalData[totalData$comp_id == '1204', ]
+#subD <- totalData[totalData$comp_id == '1204', ]
 
 # Just make sure the data set is in date order
-subD <- subD[subD$formatted_date %>%
-               as.integer %>%
-               order, ]
+#subD <- subD[subD$formatted_date %>%
+#               as.integer %>%
+#               order, ]
 
 # Get the feature sets
 original.data <- subD %>% footballstats::calculate_data()
@@ -33,11 +43,15 @@ original.data <- subD %>% footballstats::calculate_data()
 # Only look at complete rows!
 original.data %<>% subset(original.data %>% stats::complete.cases())
 
+# Drop the match IDs
 original.data <- original.data[ , 2:(original.data %>% ncol)]
 
 
 # Create scaled data set
 dataScales <- original.data %>% footballstats::get_scales()
+dataScales$commentaries <- c(
+  'shots_total', 'shots_ongoal', 'fouls', 'corners', 'possesiontime', 'yellowcards', 'saves'
+)
 save(dataScales, file = getwd() %>% paste0('/data/dataScales.rda'))
 
 # Scale the original data set
@@ -45,5 +59,5 @@ original.data %<>% footballstats::scale_data(dataScales = dataScales)
 
 # Build the neural network with scaled data
 cat(paste0(Sys.time(), ' | Building Neural Network. \n'))
-nn <- original.data[1:160, ] %>% footballstats::neural_network()
+nn <- original.data[1:210, ] %>% footballstats::neural_network()
 save(nn, file = getwd() %>% paste0('/data/nn.rda'))
