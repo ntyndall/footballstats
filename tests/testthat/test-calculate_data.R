656c2f034e1d389e996be0877eacf8a854522e61
Niall Tyndall <ntyndall@gmail.com>
2018-02-12 19:27:58 +0000

    Fixing tests!

--- tests/testthat/test-calculate_data.R
+++ tests/testthat/test-calculate_data.R
@@ -42,16 +42,29 @@ test_that('Calculate data set built from features', {
   totalData <- totalData[totalData %>% is.na %>% rowSums %>% `==`(0), ]
 
   expect_equal( totalData %>% nrow, 40 )
-  expect_equal( totalData %>% names %>% length, 11 )
-  expect_equal( totalData$shots_total %>% range, c(-22, 20) )
-  expect_equal( totalData$shots_ongoal %>% range, c(-11, 10) )
-  expect_equal( totalData$fouls %>% range, c(-10, 7) )
-  expect_equal( totalData$corners %>% range, c(-7, 9) )
-  expect_equal( totalData$possesiontime %>% range, c(-56, 56) )
-  expect_equal( totalData$yellowcards %>% range, c(-3, 3) )
-  expect_equal( totalData$saves %>% range, c(-7, 8) )
-  expect_equal( totalData$form %>% range, c(-4, 5) )
-  expect_equal( totalData$relativePos %>% range, c(-18, 19) )
+  expect_equal( totalData %>% names %>% length, 20 )
+
+  # Home details
+  expect_equal( totalData$shots_total.h %>% range, c(4, 25) )
+  expect_equal( totalData$shots_ongoal.h %>% range, c(0, 10) )
+  expect_equal( totalData$fouls.h %>% range, c(2, 18) )
+  expect_equal( totalData$corners.h %>% range, c(0, 11) )
+  expect_equal( totalData$possesiontime.h %>% range, c(22, 78) )
+  expect_equal( totalData$yellowcards.h %>% range, c(0, 4) )
+  expect_equal( totalData$saves.h %>% range, c(0, 9) )
+  expect_equal( totalData$form.h %>% range, c(0, 6) )
+  expect_equal( totalData$position.h %>% range, c(1, 20) )
+
+  # Away details
+  expect_equal( totalData$shots_total.a %>% range, c(1, 30) )
+  expect_equal( totalData$shots_ongoal.a %>% range, c(0, 14) )
+  expect_equal( totalData$fouls.a %>% range, c(4, 20) )
+  expect_equal( totalData$corners.a %>% range, c(0, 10) )
+  expect_equal( totalData$possesiontime.a %>% range, c(22, 78) )
+  expect_equal( totalData$yellowcards.a %>% range, c(0, 4) )
+  expect_equal( totalData$saves.a %>% range, c(0, 7) )
+  expect_equal( totalData$form.a %>% range, c(0, 6) )
+  expect_equal( totalData$position.a %>% range, c(1, 20) )
 
   uniqueResults <- totalData$res %>% unique
 
