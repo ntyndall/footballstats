724eaa39ae0c6ab7a7947a6a1ab9f0fc2c8607d3
Niall Tyndall <ntyndall@gmail.com>
2018-03-01 10:52:21 +0000

    Fixed bugs in creating the league table...

--- R/data_league_table.R
+++ R/data_league_table.R
@@ -36,11 +36,13 @@ create_table <- function(matchData) {
   # Loop over all match data
   for (i in 1:(matchData %>% nrow)) {
 
+    print(i)
     # Break match data down to individual slices
     slice <- matchData[i, ]
     matchID <- slice$id
     currentDate <- slice$formatted_date %>% as.integer
     teamIDs <- c(slice$localteam_id, slice$visitorteam_id)
+    teamNames <- c(slice$localteam_name, slice$visitorteam_name)
     teamScore <- slice$ft_score %>% footballstats::prs_ftscore()
     scoreDiff <- teamScore[1] - teamScore[2]
 
@@ -48,13 +50,11 @@ create_table <- function(matchData) {
     weekNum <- currentDate %>% `-`(startDate) %>% `/`(7) %>% floor %>% `+`(1)
 
     if (scoreDiff > 0) {
-      hPts <- 3
-      aPts <- 0
-    } else if (scoreDiff == 0 ) {
+      hPts <- 3; aPts <- 0
+    } else if (scoreDiff == 0) {
       hPts <- aPts <- 1
     } else {
-      hPts <- 0
-      aPts <- 3
+      hPts <- 0; aPts <- 3
     }
 
     for (j in 1:2) {
@@ -65,43 +65,42 @@ create_table <- function(matchData) {
         ) %>% as.integer %>% as.logical
 
       # Create the key if it doesnt exist
-      if (pointsToAdd && `!`(redisKey %>% rredis::redisExists())) {
+      if (pointsToAdd) {
         if (weekNum %>% `!=`(1)) {
-          # Get last match info (may have to go back multiple weeks..)
-          keepTrying <- TRUE
-          starter <- 1
-          while (keepTrying) {
-            prevKey <- paste0('cwt_l:', competitionID, ':', seasonStarting, ':', weekNum - starter, ':', teamIDs[j])
-            pRes <- prevKey %>%
-              rredis::redisHGetAll() %>%
-              lapply(as.integer)
-            if (pRes %>% length %>% `>`(0)) {
-              keepTrying <- FALSE
-            } else {
-              starter %<>% `+`(1)
-            }
-          }
+          # Get last match info
+          prevKey <- paste0('cwt_l:', competitionID, ':', seasonStarting, ':*:', teamIDs[j]) %>%
+            rredis::redisKeys()
+          oneTeamWeeks <- prevKey %>% footballstats::get_weeks()
+          prevWeek <- prevKey %>%
+            `[`(weekNum %>%
+                  `-`(oneTeamWeeks) %>%
+                  abs %>% which.min
+            )
+
+          pRes <- prevWeek %>%
+            rredis::redisHGetAll()
+          pRes$TEAM <- NULL
+          pRes %<>% lapply(as.integer)
         } else {
           pRes <- list(PTS = 0, GF = 0, GD = 0)
         }
 
-        if (j == 1) {
-          redisKey %>% rredis::redisHMSet(
-            values = list(
-              PTS = pRes$PTS %>% `+`(hPts) %>% as.character %>% charToRaw(),
-              GF = pRes$GF %>% `+`(teamScore[1]) %>% as.character %>% charToRaw(),
-              GD = pRes$GD %>% `+`(scoreDiff) %>% as.character %>% charToRaw()
-            )
-          )
+        # j = 1 for home, else away team stats
+        toAdd <- if (j == 1) {
+          list(pts = hPts, score = teamScore[1], diff = scoreDiff)
         } else {
-          redisKey %>% rredis::redisHMSet(
-            values = list(
-              PTS = pRes$PTS %>% `+`(aPts) %>% as.character %>% charToRaw(),
-              GF = pRes$GF %>% `+`(teamScore[2]) %>% as.character %>% charToRaw(),
-              GD = pRes$GD %>% `+`(-scoreDiff) %>% as.character %>% charToRaw()
-            )
-          )
+          list(pts = aPts, score = teamScore[2], diff = -scoreDiff)
         }
+
+        # Add the results to the hash
+        redisKey %>% rredis::redisHMSet(
+          values = list(
+            TEAM = teamNames[j],
+            PTS = pRes$PTS %>% `+`(toAdd$pts) %>% as.character %>% charToRaw(),
+            GF = pRes$GF %>% `+`(toAdd$score) %>% as.character %>% charToRaw(),
+            GD = pRes$GD %>% `+`(toAdd$diff) %>% as.character %>% charToRaw()
+          )
+        )
       }
     }
   }
@@ -110,18 +109,22 @@ create_table <- function(matchData) {
 #' @title Weekly Positions
 #' @export
 
+
 weekly_positions <- function(KEYS) {
 
   # Get all possible keys
   redisKeys <- paste0('cwt_l:', KEYS$COMP, ':', KEYS$SEASON, '*') %>%
     rredis::redisKeys()
 
-  # Get the unique weeks to loop over
-  weeks <- redisKeys %>%
+  # Get all unique teamIDs as a reference
+  allTeams <- redisKeys %>%
     strsplit(split = ':') %>%
-    purrr::map(4) %>%
+    purrr::map(5) %>%
     purrr::flatten_chr() %>%
-    as.integer
+    unique
+
+  # Get the unique weeks to loop over
+  weeks <- redisKeys %>% get_weeks()
 
   # Determine unique weeks and other boundaries
   uniqKeys <- weeks %>% unique %>% sort
@@ -144,9 +147,33 @@ weekly_positions <- function(KEYS) {
       purrr::map(5) %>%
       purrr::flatten_chr()
 
+    # Set an empty points list
+    singleWeek <- data.frame(stringsAsFactors = FALSE)
+
+    # Are there any teams missing??
+    allPlayed <- allTeams %in% teamIDs
+    if (allPlayed %>% all %>% `!`()) {
+      lookPrevious <- allTeams %>% subset(allPlayed %>% `!`())
+      for (j in 1:(lookPrevious %>% length)) {
+        prevWeeks <- paste0('cwt_l:', KEYS$COMP, ':', KEYS$SEASON, ':*:', lookPrevious[j]) %>%
+          rredis::redisKeys()
+        oneTeamWeeks <- prevWeeks %>% footballstats::get_weeks()
+        prevWeek <- prevWeeks %>%
+          `[`(uniqKeys[i] %>%
+                `-`(oneTeamWeeks) %>%
+                abs %>% which.min
+            )
+        subFrame <- prevWeek %>%
+          rredis::redisHGetAll() %>%
+          lapply(as.integer) %>%
+          data.frame(stringsAsFactors = FALSE)
+        subFrame$teamID <- teamIDs[j]
+        singleWeek %<>% rbind(subFrame)
+      }
+    }
+
     subKeyLen <- subKeys %>% length
 
-    singleWeek <- data.frame(stringsAsFactors = FALSE)
     for (j in 1:subKeyLen) {
       # At this point I need to order them by something!
       subFrame <- subKeys[j] %>%
@@ -173,10 +200,23 @@ weekly_positions <- function(KEYS) {
     names(position) <- singleWeek$teamID
 
     # Push list of positions to the cw_pl hashmap ...
-    paste0('cw_pl:', KEYS$COMP, ':', KEYS$SEASON, ':', uniqKeys[i]) %>%
+      paste0('cw_pl:', KEYS$COMP, ':', KEYS$SEASON, ':', uniqKeys[i]) %>%
       rredis::redisHMSet(
         values = position
       )
   }
 }
 
+#' @title Get Weeks
+#'
+#' @export
+
+
+get_weeks <- function(x) {
+  x %>%
+    strsplit(split = ':') %>%
+    purrr::map(4) %>%
+    purrr::flatten_chr() %>%
+    as.integer %>%
+    return()
+}
