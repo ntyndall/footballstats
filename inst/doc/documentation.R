d059e3fa25ab15f68c69d71545708072fdd8f18a
Niall Tyndall <ntyndall@gmail.com>
2018-03-11 12:00:52 +0000

    fix vignettes for build

--- inst/doc/documentation.R
+++ inst/doc/documentation.R
@@ -11,33 +11,63 @@ f_json <- function(y) {
   return(y %>% jsonlite::toJSON(auto_unbox = TRUE) %>% jsonlite::prettify())
 }
 
-## ------------------------------------------------------------------------
-competition.set <- "competition:set" %>% 
-  rredis::redisSMembers() %>%
-  lapply(as.character) %>%
-  first_four() %>% 
-  f_json()
+## ---- echo=FALSE---------------------------------------------------------
+#competition.set <- "competition:set" %>% 
+#  rredis::redisSMembers() %>%
+competition.set <- list(
+  "1005",
+  "1007",
+  "1093",
+  "1102",
+  "..."
+) %>% f_json()
 print(competition.set)
 
-## ------------------------------------------------------------------------
-match.info <- "csm:1204:2017:2213059" %>% 
-  rredis::redisHGetAll() %>%
-  lapply(as.character) %>% 
-  f_json()
+## ---- echo=FALSE---------------------------------------------------------
+#match.info <- "csm:1204:2017:2213059" %>% 
+#  rredis::redisHGetAll() %>%
+match.info <- list(
+  id =  "2213059",
+  comp_id = "1204",
+  formatted_date = "09.12.2017",
+  season = "2017/2018",
+  week = "16",
+  venue = "The John Smith's Stadium (Huddersfield, West Yorkshire)",
+  venue_id = "15188",
+  venue_city = "Huddersfield, West Yorkshire",
+  status = "FT",
+  timer = "",
+  time = "15:00",
+  localteam_id = "9220",
+  localteam_name = "Huddersfield Town",
+  localteam_score = "2",
+  visitorteam_id = "9065",
+  visitorteam_name = "Brighton & Hove Albion",
+  visitorteam_score = "0",
+  ht_score = "[2-0]",
+  ft_score = "[2-0]",
+  et_score = NA,
+  penalty_local = NA,
+  penalty_visitor = NA
+) %>% f_json()
 print(match.info)
 
-## ------------------------------------------------------------------------
+## ---- echo=FALSE---------------------------------------------------------
 print(list("2213173", "2213174", "2213175", "2213176", "...") %>% f_json())
 
-## ------------------------------------------------------------------------
-match.in.set <- "c_matchSetInfo:1204" %>% 
-  rredis::redisSMembers() %>%
-  lapply(as.character) %>%
-  first_four() %>% 
-  f_json()
+## ---- echo=FALSE---------------------------------------------------------
+#match.in.set <- "c_matchSetInfo:1204" %>% 
+#  rredis::redisSMembers() %>%
+match.in.set <- list(
+  "2212907",
+  "2212908",
+  "2212909",
+  "2212910",
+  "..."
+) %>% f_json()
 print(match.in.set)
 
-## ------------------------------------------------------------------------
+## ---- echo=FALSE---------------------------------------------------------
 print(list("9221", "9074", "9348", "9021", "...") %>% f_json())
 
 ## ---- echo=FALSE---------------------------------------------------------
@@ -59,7 +89,7 @@ basic.info <- list(
 ) %>% f_json()
 print(basic.info)
 
-## ------------------------------------------------------------------------
+## ---- echo=FALSE---------------------------------------------------------
 print(list("453145", "49530", "376371", "15512", "...") %>% f_json())
 
 ## ---- echo=FALSE---------------------------------------------------------
@@ -82,31 +112,45 @@ player.info <- list(
 ) 
 print(player.info %>% f_json())
 
-## ------------------------------------------------------------------------
-team.info.keys <- "cwt_l:1204:2017*" %>% 
-  rredis::redisKeys() %>% 
-  `[`(c(1:10))
-print(c(team.info.keys, "...") %>% f_json())
+## ---- echo=FALSE---------------------------------------------------------
+#team.info.keys <- "cwt_l:1204:2017*" %>% 
+#  rredis::redisKeys() %>% 
+team.info.keys <- list(
+  "cwt_l:1204:2017:12:9423",
+  "cwt_l:1204:2017:15:9158",
+  "cwt_l:1204:2017:16:9053",
+  "cwt_l:1204:2017:20:9426",
+  "..."
+) %>% f_json()
+print(team.info.keys)
 
-## ------------------------------------------------------------------------
-team.week.info <- "cwt_l:1204:2017:23:9287" %>%
-  rredis::redisHGetAll() %>% 
-  lapply(as.character)
-names(team.week.info) <- c("TEAM", "PTS", "GF", "GD")
-print(team.week.info %>% f_json())
+## ---- echo=FALSE---------------------------------------------------------
+team.week.info <- list(
+  TEAM = "Newcastle United",
+  PTS = "23",
+  GF = "21",
+  GD = "-10"
+) %>% f_json()
+print(team.week.info)
 
-## ------------------------------------------------------------------------
-league.updated <- "leagueMatchSet" %>% 
-  rredis::redisSMembers() %>%
-  lapply(as.character) %>%
-  first_four() %>% 
-  f_json()
+## ---- echo=FALSE---------------------------------------------------------
+league.updated <- list(
+  "2243999:17002", 
+  "2243932:17072", 
+  "2239831:14378", 
+  "2224810:10307",
+  "..."
+) %>% f_json()
 print(league.updated)
 
-## ------------------------------------------------------------------------
-positions <- "cw_pl:1204:2017:23" %>%
-  rredis::redisHGetAll() %>%
-  lapply(as.character) %>% 
-  f_json()
+
+## ---- echo=FALSE---------------------------------------------------------
+positions <- list(
+  `9259` = "1",
+  `9260` = "2",
+  `9249` = "3",
+  `9092` = "4",
+  `...` = "..."
+) %>% f_json()
 print(positions)
 
