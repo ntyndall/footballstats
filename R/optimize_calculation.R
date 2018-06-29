dc5fe34631f7a07ec2d93e018692d46c2129eef2
Niall Tyndall <ntyndall@gmail.com>
2018-06-29 14:22:00 +0100

    fix single row analysis (still more features to include here)

--- R/optimize_calculation.R
+++ R/optimize_calculation.R
@@ -14,18 +14,30 @@ optimize_calculation <- function(home.away.dat, day, gridPoints, gridBoundary, d
   )
 
   # Set up boundaries and constants
-  boundaries <- seq(
-    from = 1,
-    to = til + 1,
-    by = til / gridPoints
-  )
+  if (gridPoints > 0) {
+    boundaries <- seq(
+      from = 1,
+      to = til + 1,
+      by = til / gridPoints
+    )
 
-  # Grid of constants
-  mygrid <- seq(
-    from = 0.2,
-    by = - (gridBoundary / gridPoints),
-    length.out = gridPoints
-  )
+    # Grid of constants
+    mygrid <- c(
+      seq(
+        from = 1 - gridBoundary,
+        by = gridBoundary / gridPoints * 2,
+        length.out = gridPoints / 2
+      ),
+      seq(
+        from = 1 + gridBoundary,
+        by = - gridBoundary / gridPoints * 2,
+        length.out = gridPoints / 2
+      ) %>% rev
+    )
+  } else {
+    mygrid <- 0.0
+    boundaries <- c(1, til + 1)
+  }
 
   # Loop over each type of data
   for (i in 1:(frameList %>% length)) {
@@ -35,7 +47,23 @@ optimize_calculation <- function(home.away.dat, day, gridPoints, gridBoundary, d
 
     # Set up conditions for each type first
     if (i == 1) {
-      mets <- cFrame[ , c(10:15)]
+
+      uu <- list(
+        posH = cFrame$position.h,
+        posA = cFrame$position.a
+      ) %>%
+      optimize_positiongrid(
+        gridPoints = gridPoints,
+        mygrid = mygrid,
+        boundaries = boundaries
+      )
+
+      mets <- cFrame[ , c(10:15)] %>%
+        optimize_rowwise(intervals = uu) %>%
+        apply(MARGIN = 2, FUN = mean) %>%
+        t %>%
+        data.frame
+
       clinicalHome <- cFrame$localScore %>% as.integer %>% `/`(cFrame$shots_ongoal.h %>% as.integer)
       defensiveHome <- cFrame$awayScore %>% as.integer %>% `/`(cFrame$shots_ongoal.a %>% as.integer)
       dNan <- defensiveHome %>% is.nan
@@ -44,8 +72,9 @@ optimize_calculation <- function(home.away.dat, day, gridPoints, gridBoundary, d
       if (cNan %>% any) clinicalHome[cNan] <- 0.0
       mets %<>% cbind(
         data.frame(
-          `clinical.h` = clinicalHome,
-          `defensive.a` = defensiveHome,
+          `clinical.h` = clinicalHome %>% mean,
+          `defensive.a` = defensiveHome %>% mean,
+          `xg.hh` = cFrame$localScore %>% as.integer %>% mean,
           stringsAsFactors = FALSE
         )
       )
@@ -54,7 +83,25 @@ optimize_calculation <- function(home.away.dat, day, gridPoints, gridBoundary, d
     } else if (i == 3) {
       next
     } else {
-      mets %<>% cbind(cFrame[ , c(16:21)])
+
+      uu <- list(
+        posH = cFrame$position.a, # Looking at the away team so flip these for good reason!
+        posA = cFrame$position.h
+      ) %>%
+        optimize_positiongrid(
+          gridPoints = gridPoints,
+          mygrid = mygrid,
+          boundaries = boundaries
+        )
+
+      mets %<>% cbind(
+        cFrame[ , c(16:21)] %>%
+          optimize_rowwise(intervals = uu) %>%
+          apply(MARGIN = 2, FUN = mean) %>%
+          t %>%
+          data.frame
+      )
+
       defensiveAway <- cFrame$localScore %>% as.integer %>% `/`(cFrame$shots_ongoal.h %>% as.integer)
       clinicalAway <- cFrame$awayScore %>% as.integer %>% `/`(cFrame$shots_ongoal.a %>% as.integer)
       dNan <- defensiveAway %>% is.nan
@@ -63,14 +110,15 @@ optimize_calculation <- function(home.away.dat, day, gridPoints, gridBoundary, d
       if (cNan %>% any) clinicalAway[cNan] <- 0.0
       mets %<>% cbind(
         data.frame(
-          `clinical.h` = clinicalAway,
-          `defensive.a` = defensiveAway,
+          `clinical.h` = clinicalAway %>% mean,
+          `defensive.a` = defensiveAway %>% mean,
+          `xg.aa` = cFrame$awayScore %>% as.integer %>% mean,
           stringsAsFactors = FALSE
         )
       )
     }
-   # inters <- findInterval(cFrame$position.h, boundaries)
   }
 
-
+  # Return row of data frame here
+  return(mets)
 }
