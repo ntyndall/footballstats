e5da5b9339754bc082cbe2c3fe6941aec3f10530
Niall Tyndall <ntyndall@gmail.com>
2018-07-05 18:42:14 +0100

    Remove the basic stats from the feature set and tidy up the code a little to a working state.

--- R/optimize_calculation.R
+++ R/optimize_calculation.R
@@ -39,6 +39,7 @@ optimize_calculation <- function(home.away.dat, day, gridPoints, gridBoundary, d
     boundaries <- c(1, til + 1)
   }
 
+
   # Loop over each type of data
   for (i in 1:(frameList %>% length)) {
 
@@ -48,33 +49,24 @@ optimize_calculation <- function(home.away.dat, day, gridPoints, gridBoundary, d
     # Set up conditions for each type first
     if (i == 1) {
 
-      # Calculate decay Factors
-      decayFactors <- cFrame %>%
-        nrow %>%
-        footballstats::optimize_decay(
-          decay = decayFactor
-        )
+      # Create feature data set
+      analyse.data <- cFrame %>% footballstats::create_feature_data()
 
-      # Just home
-      first <- list(
+      # Get position list
+      positions <- list(
         posH = cFrame$position.h,
         posA = cFrame$position.a
-      ) %>%
+      )
+
+      # Just home
+      first <- positions %>%
         footballstats::optimize_get_data(
-          cData = cFrame[ , c(10:15)],
+          cData = analyse.data,
           gridPoints = gridPoints,
           mygrid = mygrid,
           boundaries = boundaries,
-          decayFactors = decayFactors
+          decayFactor = decayFactor
         )
-
-      first %<>% cbind(
-        data.frame(
-          `xg.h` = cFrame$localScore %>% as.integer %>% mean,
-          `form.h` = cFrame$result %>% footballstats::form_to_int(),
-          stringsAsFactors = FALSE
-        )
-      )
 
       # Home and away
       if (totalPer > 0.01) {
@@ -90,60 +82,23 @@ optimize_calculation <- function(home.away.dat, day, gridPoints, gridBoundary, d
       } else {
         mets <- first
       }
-
-      clinicalHome <- cFrame$localScore %>% as.integer %>% `/`(cFrame$shots_ongoal.h %>% as.integer) %>% pmin(1)
-      defensiveHome <- cFrame$awayScore %>% as.integer %>% `/`(cFrame$shots_ongoal.a %>% as.integer) %>% pmin(1)
-      dNan <- defensiveHome %>% is.nan
-      cNan <- clinicalHome %>% is.nan
-      if (dNan %>% any) defensiveHome[dNan] <- 0.0
-      if (cNan %>% any) clinicalHome[cNan] <- 0.0
-      mets %<>% cbind(
-        data.frame(
-          `clinical.hh` = clinicalHome %>% mean,
-          `defensive.ha` = defensiveHome %>% mean,
-          stringsAsFactors = FALSE
-        )
-      )
     } else if (i == 2) {
       next
     } else if (i == 3) {
       next
     } else {
-
-      # Calculate decay Factors
-      decayFactors <- cFrame %>%
-        nrow %>%
-        footballstats::optimize_decay(
-          decay = decayFactor
-        )
+      # Create feature data set
+      analyse.data <- cFrame %>% footballstats::create_feature_data(type = 'a')
 
       # Just away
-      second <- list(
-        posH = cFrame$position.a,
-        posA = cFrame$position.h
-      ) %>%
+      second <- positions %>%
         footballstats::optimize_get_data(
-          cData = cFrame[ , c(16:21)],
+          cData = analyse.data,
           gridPoints = gridPoints,
           mygrid = mygrid,
           boundaries = boundaries,
-          decayFactors = decayFactors
+          decayFactor = decayFactor
         )
-
-      # Readjust results...
-      cRes <- cFrame$result
-      newRes <- c()
-      for (i in 1:(cRes %>% length)) {
-        newRes %<>% c(if (cRes[i] == 'W') 'L' else if (cRes[i] == 'L') 'W' else 'D')
-      }
-
-      second %<>% cbind(
-        data.frame(
-          `xg.a` = cFrame$awayScore %>% as.integer %>% mean,
-          `form.a` = newRes %>% footballstats::form_to_int(),
-          stringsAsFactors = FALSE
-        )
-      )
 
       # Home and away
       if (totalPer > 0.01) {
@@ -160,20 +115,6 @@ optimize_calculation <- function(home.away.dat, day, gridPoints, gridBoundary, d
       } else {
         mets %<>% cbind(second)
       }
-
-      defensiveAway <- cFrame$localScore %>% as.integer %>% `/`(cFrame$shots_ongoal.h %>% as.integer) %>% pmin(1)
-      clinicalAway <- cFrame$awayScore %>% as.integer %>% `/`(cFrame$shots_ongoal.a %>% as.integer) %>% pmin(1)
-      dNan <- defensiveAway %>% is.nan
-      cNan <- clinicalAway %>% is.nan
-      if (dNan %>% any) defensiveAway[dNan] <- 0.0
-      if (cNan %>% any) clinicalAway[cNan] <- 0.0
-      mets %<>% cbind(
-        data.frame(
-          `clinical.ah` = clinicalAway %>% mean,
-          `defensive.aa` = defensiveAway %>% mean,
-          stringsAsFactors = FALSE
-        )
-      )
     }
   }
 
@@ -186,7 +127,7 @@ optimize_calculation <- function(home.away.dat, day, gridPoints, gridBoundary, d
 #' @export
 
 
-optimize_get_data <- function(positions, cData, gridPoints, mygrid, boundaries, decayFactors) {
+optimize_get_data <- function(positions, cData, gridPoints, mygrid, boundaries, decayFactor) {
   uu <- positions %>%
     footballstats::optimize_positiongrid(
       gridPoints = gridPoints,
@@ -194,12 +135,17 @@ optimize_get_data <- function(positions, cData, gridPoints, mygrid, boundaries, 
       boundaries = boundaries
     )
 
-  # Reverse the factors
-  decayFactors %<>% rev
+  # Calculate decay Factors
+  decayFactors <- cData %>%
+    nrow %>%
+    footballstats::optimize_decay(
+      decay = decayFactor
+    ) %>%
+    rev
 
   # Apply the factors and summarise data
   summarisedRes <- cData %>%
-    footballstats::optimize_rowwise(intervals = uu) %>%
+    apply(MARGIN = 2, FUN = function(x) x * uu) %>%
     apply(MARGIN = 2, FUN = function(x) x * decayFactors) %>%
     apply(MARGIN = 2, FUN = sum) %>%
     t %>%
@@ -219,13 +165,6 @@ optimize_sort_ha <- function(homeDat, awayDat, gridPoints, mygrid, boundaries, h
   # Remove til
   homeDat$til <- NULL
 
-  # Readjust results...
-  cRes <- awayDat$result
-  newRes <- c()
-  for (i in 1:(cRes %>% length)) {
-    newRes %<>% c(if (cRes[i] == 'W') 'L' else if (cRes[i] == 'L') 'W' else 'D')
-  }
-
   # Bluff home / away just to get the correct metrics! (as home away doesnt matter now)
   newAway <- data.frame(
     matchID = awayDat$matchID,
@@ -236,7 +175,7 @@ optimize_sort_ha <- function(homeDat, awayDat, gridPoints, mygrid, boundaries, h
     awayID = awayDat$localID,
     localScore = awayDat$awayScore,
     awayScore = awayDat$localScore,
-    result = newRes,
+    result = awayDat$result %>% footballstats::flip_res(),
     stringsAsFactors = FALSE
   )
 
@@ -259,36 +198,112 @@ optimize_sort_ha <- function(homeDat, awayDat, gridPoints, mygrid, boundaries, h
     )
   )
 
+  # Order the data frame
   orderDat <- rbind(homeDat, newAway)
   orderDat <- orderDat[orderDat$date %>% order, ]
 
-  # Calculate decay Factors
-  decayFactors <- orderDat %>%
-    nrow %>%
-    footballstats::optimize_decay(
-      decay = decayFactor
-    )
+  # Get metrics back
+  analyse.data <- orderDat %>%
+    footballstats::create_feature_data()
 
+  # Rename if looking at away team
+  # (it is fudged to consider home but needs changed back now)
+  if (ha == 'a') {
+    names(analyse.data) <- analyse.data %>%
+      names %>%
+      strsplit(split = '[.]') %>%
+      purrr::map(1) %>%
+      purrr::flatten_chr() %>%
+      paste0('.a')
+  }
+
+  # Adjust with factors
   newDat <- list(
     posH = orderDat$position.h,
     posA = orderDat$position.a
   ) %>%
     footballstats::optimize_get_data(
-      cDat = orderDat[ , c(10:15)],
+      cDat = analyse.data,
       gridPoints = gridPoints,
       mygrid = mygrid,
       boundaries = boundaries,
-      decayFactors = decayFactors
+      decayFactor = decayFactor
   )
-
-  other <- data.frame(
-    orderDat$localScore %>% as.integer %>% mean,
-    orderDat$result %>% footballstats::form_to_int(),
-    stringsAsFactors = FALSE
-  )
-  names(other) <- paste0(c('xg.', 'form.'), ha)
-  newDat %<>% cbind(other)
 
   return(newDat)
 }
 
+#' @title Flip Result
+#'
+#' @export
+
+
+flip_res <- function(x) {
+  # Readjust results...
+  cRes <- x
+  newRes <- c()
+  for (i in 1:(cRes %>% length)) {
+    newRes %<>% c(if (cRes[i] == 'W') 'L' else if (cRes[i] == 'L') 'W' else 'D')
+  }
+  return(newRes)
+}
+
+#' @title Create Feature Data
+#'
+#' @export
+
+
+create_feature_data <- function(cFrame, type = 'h') {
+
+  # Make sure to get the right information
+  goals <- if (type == 'h') cFrame$localScore else cFrame$awayScore
+  ftRes <- if (type == 'h') cFrame$result else cFrame$result %>% footballstats::flip_res()
+  oGoals <- if (type == 'h') cFrame$awayScore else cFrame$localScore
+  oType <- if (type == 'h') 'a' else 'h'
+
+
+  featFrame <- data.frame(
+    xg = goals %>% as.integer,
+    form = ftRes %>% footballstats::formint_2(),
+    clinical = goals %>% footballstats::take_ratio(y = cFrame[[paste0('shots_ongoal.', type)]]),
+    defensive = oGoals %>% footballstats::take_ratio(y = cFrame[[paste0('shots_ongoal.', oType)]]),
+    shotacc = cFrame[[paste0('shots_ongoal.', type)]] %>% footballstats::take_ratio(y = cFrame[[paste0('shots_total.', type)]]),
+    shotrate = cFrame[[paste0('shots_total.', type)]] %>% footballstats::take_ratio(y = cFrame[[paste0('possesiontime.', type)]]),
+    stringsAsFactors = FALSE
+  )
+
+  names(featFrame) <- featFrame %>% names %>% paste0('.', type)
+  return(featFrame)
+}
+
+#' @title Form 2 Int
+#'
+#' @export
+
+
+formint_2 <- function(oldForms, winPoints = 2, drawPoints = 1, losePoints = 0) {
+  newForms <- c()
+  for (p in 1:(oldForms %>% length)) {
+    newForms %<>% c(if (oldForms[p] == 'W') 2 else if (oldForms[p] == 'D') 1 else 0)
+  }
+  return(newForms)
+}
+
+
+#' @title Take Ratio
+#'
+#' @export
+
+
+take_ratio <- function(x, y) {
+
+  # Replace NaN with 0
+  repl_nan <- function(x) {
+    anyNan <- x %>% is.nan
+    if (anyNan %>% any) x[anyNan] <- 0.0
+    return(x)
+  }
+
+  # Calculate ratio
+  return(x %>% as.integer %>% `/`(y %>% as.integer) %>% pmin(1) %>% repl_nan())
+}
