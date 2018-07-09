dd3f5b75b1bdc5ed32e7149908278258f103a006
Niall Tyndall <ntyndall@gmail.com>
2018-07-09 12:11:48 +0100

    Check a data frame if a row exists from another row of a data frame (this is for separate runs of the optimization procedure.

--- R/optimize_utilities.R
+++ R/optimize_utilities.R
@@ -48,3 +48,27 @@ optimize_positiongrid <- function(pos, gridPoints, mygrid, boundaries) {
   }
   return(results)
 }
+
+#' @title Get Grid Matches
+#'
+#' @export
+
+
+get_grid_matches <- function(existing.metrics, fullGrid, r = FALSE) {
+  # Make sure the grid names matches up
+  names(fullGrid) <- names(existing.metrics)
+
+  # Check for matches
+  totMatches <- 0
+  for (i in 1:(existing.metrics %>% nrow)) {
+    res <- suppressMessages(
+      expr = fullGrid %>%
+        plyr::match_df(existing.metrics[i, ]) %>%
+        nrow
+    )
+    if (res %>% `==`(1)) if (r) return(TRUE) else totMatches %<>% `+`(1)
+  }
+
+  # Return number of matches
+  return(if (r) FALSE else totMatches)
+}
