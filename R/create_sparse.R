5ce5d70a42b8477b18546a6e256540fa0e4ab781
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-07-23 15:27:19 +0100

    Abstract functionality for creating a sparse matrix of some internal datasets

---
+++ R/create_sparse.R
@@ -0,0 +1,36 @@
+#' @title Create Sparse
+#'
+#' @description A function used for creating a sparse matrix
+#'  with \code{(ncol - 1)} features and \code{1} column of
+#'  results, i.e. W / L / D.
+#'
+#' @export
+
+
+create_sparse <- function(my.data, boundLen) {
+  myDimNames <- indI <- indJ <- c()
+  allNames <- my.data %>%
+    colnames %>%
+    `[`(1:(train.data %>% ncol %>% `-`(1)))
+  startLen <- 0
+
+  for (k in 1:(my.data %>% ncol %>% `-`(1))) {
+    for (j in 1:(my.data %>% nrow)) {
+      indJ %<>% c(startLen + my.data[j, k])
+      indI %<>% c(j)
+    }
+    myDimNames %<>% c(allNames[k] %>% paste0('=', c(1:boundLen)))
+    startLen %<>% `+`(boundLen)
+  }
+
+  # Return the sparse matrix
+  # How do I ensure there are trailing zeros?
+  return(
+    Matrix::sparseMatrix(
+      i = indI,
+      j = indJ,
+      x = 1,
+      dimnames = list(NULL, myDimNames)
+    )
+  )
+}
