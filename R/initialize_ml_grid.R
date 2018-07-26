8fcd04078f6df2128f71449155f18ce1a4ce96b2
Niall Tyndall <ntyndall@gmail.com>
2018-07-26 14:11:14 +0100

    Setting up the grid (first stab)

---
+++ R/initialize_ml_grid.R
@@ -0,0 +1,25 @@
+#' @title Initialize ML Grid
+#'
+#' @description A function for returning the grid to be used
+#'  in optimizing the ML models.
+#'
+#' @export
+
+
+initialize_ml_grid <- function() {
+  return(
+    list(
+      DAYS = c(3, 4, 5),
+      GRID_PTS = c(2, 4, 6),
+      GRID_BOUND = c(0.1, 0.2),
+      DECAY = c(1, 5000),
+      TOTAL_PERC = seq(from = 0.0, to = 1.0, by = 1),
+      NN_REP = 1,
+      NN_THRESH = 0.18,
+      XG_ROUNDS = 5000,
+      XG_DEPTH = 10,
+      XG_ETA = 0.2,
+      XG_GAMMA = 2
+    )
+  )
+}
