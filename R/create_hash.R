fcbbbea025563f979780387d66f6413d11d2552f
Niall Tyndall <ntyndall@gmail.com>
2018-08-02 20:24:58 +0100

    If raw objects exist in redis then need to handle them correctly.

--- R/create_hash.R
+++ R/create_hash.R
@@ -4,6 +4,17 @@
 
 
 create_hash <- function(x) {
+
+  # Check if any are raw
+  anyRaw <- x %>%
+    lapply(typeof) %>%
+    purrr::map(function(z) z %>% `==`("raw")) %>%
+    purrr::flatten_lgl()
+
+  # If raw then replace with character
+  if (anyRaw %>% any) x[anyRaw] %<>% lapply(redux::bin_to_object) %>% lapply(as.character)
+
+  # Now start to flatten and rename
   xVec <- x %>% purrr::flatten_chr()
   xList <- xVec %>%
     `[`(c(FALSE, TRUE)) %>%
