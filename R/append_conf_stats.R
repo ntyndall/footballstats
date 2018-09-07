6e94ff5b5c6e866e8988e99f9d23f9bb2b780c36
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-09-07 14:28:18 +0100

    Just spacing

--- R/append_conf_stats.R
+++ R/append_conf_stats.R
@@ -9,7 +9,7 @@ append_conf_stats <- function(totalStats, new.odds, Actual.score, Predicted.scor
   resultTable <- table(Actual.score, Predicted.score)
   rt <- caret::confusionMatrix(data = resultTable)
 
-# Refine stats here from the confusion matrix
+  # Refine stats here from the confusion matrix
   oStats <- rt$overall[c('Accuracy', 'AccuracyLower', 'AccuracyUpper')] %>% as.double
   oSens <- if (resultTable %>% nrow %>% `>`(2)) {
     rt$byClass[1:3, 'Sensitivity'] %>% as.double
