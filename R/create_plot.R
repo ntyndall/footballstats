8022e8757a31d93983f15bec907045189f05d115
Niall Tyndall <niall.tyndall@cyberlytic.com>
2018-09-18 10:54:24 +0100

    No coverage for plots again..

--- R/create_plot.R
+++ R/create_plot.R
@@ -58,7 +58,7 @@ create_plot <- function(total.results, day, gridPoints, gridBoundary, decayFacto
   for (i in 1:toplot) {
 
     # Only create plot if required
-    if (savePlot) {
+    if (savePlot) { # start nocov
       # Define the plot title
       plotTitle <- paste0(
         "day : ", day, " | gridP : ", gridPoints,
@@ -79,7 +79,7 @@ create_plot <- function(total.results, day, gridPoints, gridBoundary, decayFacto
         filename = dirNames[i] %>% paste0("/", fileName, ".png"),
         plot = g
       )
-    }
+    } # end nocov
 
     # Define current values of feature
     vals <- total.results[[singlePlots[i]]]
