f19b6ea46d0533c6b8e98046210fc63ecb12d188
Niall Tyndall <niall.tyndall@cyberlytic.com>
2019-01-28 18:46:30 +0000

    Push the decision of whether the data can be grouped in blocks of  to a separate function!

---
+++ R/sub_metrics.R
@@ -0,0 +1,105 @@
+#' @title Sub Metrics
+#'
+#' @export
+
+
+sub_metrics <- function(total.metrics, colNames, odds.frame = data.frame()) {
+  odds.results <- total.results <- data.frame(stringsAsFactors = FALSE)
+  allMatchIDs <- c()
+
+  # Set up a progress bar here
+  pb <- utils::txtProgressBar(
+    min = 0,
+    max = total.metrics %>% nrow,
+    style = 3
+  )
+
+  # Now loop over all of total.metrics
+  for (drow in 2:(total.metrics %>% nrow)) {
+
+    # Update the progress bar
+    utils::setTxtProgressBar(
+      pb = pb,
+      value = drow
+    )
+
+    current.row <- total.metrics[drow, ]
+    smaller.metrics <- total.metrics[1:(drow - 1), ]
+
+    # Subset smaller subset for logical matches
+    haMatches <- list(
+      hh = smaller.metrics[[colNames$localID]] %>% `==`(current.row[[colNames$localID]]),
+      ah = smaller.metrics[[colNames$awayID]] %>% `==`(current.row[[colNames$localID]]),
+      ha = smaller.metrics[[colNames$localID]] %>% `==`(current.row[[colNames$awayID]]),
+      aa = smaller.metrics[[colNames$awayID]] %>% `==`(current.row[[colNames$awayID]])
+    )
+
+    # Number of rows of each type
+    allSums <- haMatches %>% purrr::map(sum)
+
+    if (allSums %>% purrr::map(function(x) x > DAYS[i]) %>% purrr::flatten_lgl() %>% all) {
+
+      # Separating function
+      sep_dat <- function(x, d, s) return(x[(s - d + 1):(x %>% nrow), ])
+
+      # Get grid values here
+      home.away.dat <- rbind(
+        smaller.metrics %>% subset(haMatches$hh) %>% sep_dat(d = DAYS[i], s = allSums$hh),
+        smaller.metrics %>% subset(haMatches$ah) %>% sep_dat(d = DAYS[i], s = allSums$ah),
+        smaller.metrics %>% subset(haMatches$ha) %>% sep_dat(d = DAYS[i], s = allSums$ha),
+        smaller.metrics %>% subset(haMatches$aa) %>% sep_dat(d = DAYS[i], s = allSums$aa)
+      )
+
+      # do calculations here
+      result.dat <- home.away.dat %>%
+        footballstats::optimize_calculation(
+          day = DAYS[i],
+          gridPoints = GRID_PTS[j],
+          gridBoundary= GRID_BOUND[k],
+          decayFactor = DECAY[l],
+          til = current.row$til,
+          totalPer = TOTAL_PERC[m]
+        )
+
+      # Append positions on
+      result.dat$`position.h` <- current.row$`position.h` %>% `/`(current.row$til)
+      result.dat$`position.a` <- current.row$`position.a` %>% `/`(current.row$til)
+      result.dat$res <- current.row$result
+      total.results %<>% rbind(result.dat)
+      allMatchIDs %<>% c(current.row$matchID)
+
+      # Make sure there is a match, if not then set as NA
+      matchingIndex <- current.row$matchID %>% `==`(odds.frame$matchID)
+      odds.results %<>% rbind(
+        if (matchingIndex %>% any) {
+          odds.frame[matchingIndex %>% which, ]
+        } else {
+          data.frame(
+            matchID = current.row$matchID,
+            homewin = NA,
+            draw = NA,
+            awaywin = NA,
+            stringsAsFactors = FALSE
+          )
+        }
+      )
+    } else {
+      next
+    }
+  }
+
+  # Match up matchIDs with odds frame
+  new.odds <- odds.frame[allMatchIDs %>% match(odds.frame$matchID), ]
+
+  # Remove NA's from matchIDs
+  allMatchIDs %<>%
+    `[`(total.results %>% stats::complete.cases())
+
+  return(
+    list(
+      data = total.results,
+      odds = new.odds,
+      matchIDs = allMatchIDs
+    )
+  )
+}
